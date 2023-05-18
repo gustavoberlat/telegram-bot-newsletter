224f0d7b3c3297c06c88688f6fcbe854214b3d66
gr4yt3x <fortifixb@gmail.com>
2023-05-18 01:15:07 -0300

    piloto

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const axios = require("axios");
+
+
+require('dotenv').config();
+
+
+const token = process.env.BOT_TOKEN
+const chatId = process.env.CHAT_ID 
+
+
+async function sendTelegramMessage(message){
+	const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}`
+
+	axios.get(url)
+}
+
+
+async function chatGPTRequest() {
+	await axios.post('https://free.churchless.tech/v1/chat/completions', {
+  model: 'gpt-3.5-turbo',
+  messages: [{ role: 'user', content: 'Me fale uma curiosidade legal sobre a inglaterra.' }]
+}, {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).then(response => {
+	const result = response.data
+	const message = result.choices[0].message.content
+	sendTelegramMessage(message)
+})
+}
+
+
+
+app.get('/try',(req) => {
+	console.log(req)
+})
+
+
+
+return {"fail": false}
+
+
+
+
